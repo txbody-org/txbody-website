c9507d3ec04ed1aaab91bb07ee5099024c697780
Dipesh Chaulagain <dcdipesh1998@gmail.com>
2025-09-07 09:41:09 +0545

    Updated vite.config.ts to include prerendering plugin for build command with specified routes and renderer options.

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,29 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import prerender from "@prerenderer/rollup-plugin";
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ command }) => ({
+  plugins: [
+    react(),
+    tailwindcss(),
+    ...(command === "build"
+      ? [
+          prerender({
+            routes: ["/"],
+            renderer: "@prerenderer/renderer-puppeteer",
+            rendererOptions: {
+              headless: true,
+              args: ["--no-sandbox", "--disable-setuid-sandbox"],
+            },
+          }),
+        ]
+      : []),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   base: "/",
-});
+}));
